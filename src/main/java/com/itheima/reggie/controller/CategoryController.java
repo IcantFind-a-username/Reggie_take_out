85de036f159ad888b8f5b95615204dfffde167a4
franz <franzxu28@gmail.com>
2025-02-25 19:54:49 +0100

    regular update

---
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -0,0 +1,44 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping
+    public R<String> save(@RequestBody Category category) {
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize) {
+        Page<Category> pageInfo = new Page<>(page, pageSize);
+        LambdaQueryWrapper<Category> wrapper = new LambdaQueryWrapper<>();
+        wrapper.orderByAsc(Category::getSort);
+        categoryService.page(pageInfo, wrapper);
+        return R.success(pageInfo);
+    }
+
+    @DeleteMapping
+    public R<String> delete(@RequestParam(value = "ids") Long id){
+//        categoryService.removeById(id);
+        categoryService.remove(id);
+        return R.success("分类信息删除成功");
+    }
+
+    @PutMapping
+    public R<String> update(@RequestBody Category category) {
+        categoryService.updateById(category);
+        return R.success("分类信息更新成功");
+    }
+}
